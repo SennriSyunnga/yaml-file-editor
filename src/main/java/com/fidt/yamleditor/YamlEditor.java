405aedab7422b5c7fa00c62e1d5b83136b7296f5
Sennri <nnnnnn_2657@qq.com>
2020-10-29 23:35:20 +0800

    现版本似乎第一个测试有问题，需要debug。

--- src/main/java/com/fidt/yamleditor/YamlEditor.java
+++ src/main/java/com/fidt/yamleditor/YamlEditor.java
@@ -1,6 +1,5 @@
 package com.fidt.yamleditor;
 
-import lombok.Cleanup;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -49,58 +48,98 @@ public class YamlEditor {
         return yamls;
     }
 
-    //这里有个问题，如果这个object没有名字呢？
+
+//    /**
+//     * @param key     key格式：aaa.bbb.ccc (如果对于下面是数组的情况呢？
+//     * @param yamlMap
+//     * @return java.lang.Object
+//     * @Param
+//     * @Author Sennri
+//     * @Description 通过properties的方式获取yaml中的属性值
+//     * @Date 2020/10/29 15:06
+//     **/
+//    public static Object getValue(String key, Map<String, Object> yamlMap) {
+//        if (key.contains(".")) {
+//            String[] keys = key.split("[.]");
+//            Object object = yamlMap.get(keys[0]);
+//            if (object instanceof Map) {
+//                return getValue(key.substring(key.indexOf(".") + 1), (Map<String, Object>) object);
+//            } else if (object instanceof List) {
+//                return getValue(key.substring(key.indexOf(".") + 1), (List<Object>) object);
+//            } else {
+//                return null;
+//            }
+//        } else {
+//            return yamlMap.get(key);
+//        }
+//    }
+//
+//    /**
+//     * @param key
+//     * @param yamlList
+//     * @return java.lang.Object
+//     * @Param
+//     * @Author DELL
+//     * @Description 不支持
+//     * @Date 2020/10/29 15:22
+//     **/
+//
+//    public static Object getValue(String key, List<Object> yamlList) {
+//        if (key.contains(".")) {
+//            String[] keys = key.split("[.]");
+//            Object object = yamlList.get(Integer.parseInt(keys[0]));
+//            if (object instanceof Map) { //是Map
+//                return getValue(key.substring(key.indexOf(".") + 1), (Map<String, Object>) object);
+//            } else if (object instanceof List) { //也可能是链表
+//                return getValue(key.substring(key.indexOf(".") + 1), (List<Object>) object);
+//            } else {
+//                return null;
+//            }
+//        } else {
+//            return yamlList.get(Integer.parseInt(key));
+//        }
+//    }
 
     /**
-     * @param key     key格式：aaa.bbb.ccc (如果对于下面是数组的情况呢？
-     * @param yamlMap
-     * @return java.lang.Object
-     * @Param
      * @Author Sennri
-     * @Description 通过properties的方式获取yaml中的属性值
-     * @Date 2020/10/29 15:06
-     **/
-    public static Object getValue(String key, Map<String, Object> yamlMap) {
-        if (key.contains(".")) {
-            String[] keys = key.split("[.]");
-            Object object = yamlMap.get(keys[0]);
-            if (object instanceof Map) {
-                return getValue(key.substring(key.indexOf(".") + 1), (Map<String, Object>) object);
-            } else if (object instanceof List) {
-                return getValue(key.substring(key.indexOf(".") + 1), (List<Object>) object);
-            } else {
-                return null;
-            }
-        } else {
-            return yamlMap.get(key);
-        }
-    }
-
-    /**
+     * @Description 泛型化上面两个函数
+     * @Date 2020/10/29 23:05
+     * @ParamList:
      * @param key
-     * @param yamlList
+     * @param target
      * @return java.lang.Object
-     * @Param
-     * @Author DELL
-     * @Description 不支持
-     * @Date 2020/10/29 15:22
-     **/
-
-    public static Object getValue(String key, List<Object> yamlList) {
-        if (key.contains(".")) {
-            String[] keys = key.split("[.]");
-            Object object = yamlList.get(Integer.parseInt(keys[0]));
-            if (object instanceof Map) { //是Map
-                return getValue(key.substring(key.indexOf(".") + 1), (Map<String, Object>) object);
-            } else if (object instanceof List) { //也可能是链表
-                return getValue(key.substring(key.indexOf(".") + 1), (List<Object>) object);
+     */
+    public static Object getValue(String key, Object target) throws Exception {
+        if (target instanceof List){
+            if (key.contains(".")) {
+                String[] keys = key.split("[.]");
+                Object object = ((List)target).get(Integer.parseInt(keys[0]));
+                if (object instanceof Map || object instanceof List) { //是Map
+                    return getValue(key.substring(key.indexOf(".") + 1), object);
+                } else {
+                    return null;
+                }
             } else {
-                return null;
+                return ((List)target).get(Integer.parseInt(key));
             }
-        } else {
-            return yamlList.get(Integer.parseInt(key));
+        }
+        else if(target instanceof Map){
+            if (key.contains(".")) {
+                String[] keys = key.split("[.]");
+                Object object = ((Map) target).get(keys[0]);
+                if (object instanceof Map || object instanceof List) {
+                    return getValue(key.substring(key.indexOf(".") + 1), object);
+                } else {
+                    return null;
+                }
+            } else {
+                return ((Map) target).get(key);
+            }
+        }else{
+            throw new Exception("Collection type error");
         }
     }
+
 
 
     // TODO: 2020/10/23 这里应该有问题,并不能解决如果下面是一个数组的情况，但是目前应该还没遇到这种情况
@@ -114,58 +153,109 @@ public class YamlEditor {
      * @Description 使用递归的方式设置map中的值，仅适合单一属性 key的格式: "server.port"
      * @Date 2020/10/29 15:42
      **/
-    public Map<String, Object> setValue(String key, Object value) {
+    public Map<String, Object> generateMap(String key, Object value) {
         Map<String, Object> result = new LinkedHashMap<>();
         String[] keys = key.split("[.]");
-        int i = keys.length - 1;
-        result.put(keys[i], value);
-        if (i > 0) {
-            return setValue(key.substring(0, key.lastIndexOf(".")), result);
+        result.put(keys[keys.length - 1], value);
+        if (keys.length  > 1) {
+            return generateMap(key.substring(0, key.lastIndexOf(".")), result);
         }
         return result;
     }
 
+//    /**
+//     * @param map
+//     * @param key
+//     * @param value
+//     * @return java.util.Map<java.lang.String, java.lang.Object>
+//     * @Param
+//     * @Author DELL
+//     * @Description 这个没有考虑有list的情况
+//     * @Date 2020/10/29 15:40
+//     **/
+//
+//
+//    public boolean setValue(Map<String, Object> map, String key, Object value) {
+//        String[] keys = key.split("[.]");
+//        Map temp = map;
+//        for (int i = 0; i < keys.length - 1; i++) {
+//            if (temp.containsKey(keys[i])) {
+//                temp = (Map) temp.get(keys[i]);
+//            } else {
+//                return false;
+//            }
+//        }
+//        temp.put(keys[keys.length - 1], value);//即便没有这个值
+//        return true; // 设置成功
+//    }
+    
+    
+    
     /**
+     * @Author Sennri
+     * @Description 
+     * @Date 2020/10/29 20:40
+     * @ParamList: 
      * @param map
      * @param key
      * @param value
-     * @return java.util.Map<java.lang.String, java.lang.Object>
-     * @Param
-     * @Author DELL
-     * @Description 这个没有考虑有list的情况
-     * @Date 2020/10/29 15:40
-     **/
-
-
+     * @return boolean
+     */
     public boolean setValue(Map<String, Object> map, String key, Object value) {
         String[] keys = key.split("[.]");
-        Map temp = map;
-        for (int i = 0; i < keys.length - 1; i++) {
-            if (temp.containsKey(keys[i])) {
-                temp = (Map) temp.get(keys[i]);
+        if (keys.length == 1) {
+            map.put(keys[keys.length - 1], value);//即便没有这个值
+            return true; // 设置成功
+        } else {
+            Object object;
+            if ((object = map.get(Integer.parseInt(keys[0]))) != null) {
+                if (object instanceof Map)
+                    return setValue((Map<String, Object>) object, key, value);
+                else if (object instanceof List)
+                    return setValue((List<Object>) object, key, value);
+                else {
+                    System.out.println("Input key error");
+                    return false;
+                }
             } else {
+                System.out.println("Can't find this key");
                 return false;
             }
         }
-        temp.put(keys[keys.length - 1], value);//即便没有这个值
-        return true; // 设置成功
     }
 
-    public boolean setValue(Collection map, String key, Object value) {
+    /**
+     * @Author Sennri
+     * @Description 
+     * @Date 2020/10/29 20:40
+     * @ParamList: 
+     * @param list
+     * @param key
+     * @param value
+     * @return boolean
+     */
+    public boolean setValue(List<Object> list, String key, Object value) {
         String[] keys = key.split("[.]");
-        Collection temp = map;
-        for (int i = 0; i < keys.length - 1; i++) {
-            if (temp.containsKey(keys[i])) {
-                temp = (Map) temp.get(keys[i]);
+        if (keys.length == 1) {
+            list.set(Integer.parseInt(keys[0]), value);//即便没有这个值
+            return true; // 设置成功
+        } else {
+            Object object;
+            if ((object = list.get(Integer.parseInt(keys[0]))) != null) {
+                if (object instanceof Map)
+                    return setValue((Map<String, Object>) object, key, value);
+                else if (object instanceof List)
+                    return setValue((List<Object>) object, key, value);
+                else {
+                    System.out.println("Input key error");
+                    return false;
+                }
             } else {
+                System.out.println("Can't find this key");
                 return false;
             }
         }
-        temp.put(keys[keys.length - 1], value);//即便没有这个值
-        return true; // 设置成功
     }
-
-
 
 
     /**
@@ -176,16 +266,17 @@ public class YamlEditor {
      * @param yamlName
      * @return true 修改成功，false 修改失败。
      */
-    public boolean updateYaml(String key, @Nullable Object value, String yamlName) {
+    public boolean updateYaml(String key, @Nullable Object value, String yamlName) throws Exception {
 
         Map<String, Object> yamlToMap = this.getYamlToMap(yamlName);
         if (null == yamlToMap) {
             return false;
         }
+        
+        // 只返回倒数第二级
+        Object target = getValue(key.substring(0, key.lastIndexOf(".")), yamlToMap); //上一级map
+        Object oldVal = getValue(key.substring(key.lastIndexOf(".")+1) , target); // 对上一级map取key值，得到value
 
-        if
-        // 如果getValue不返回最高一级，只返回倒数第二级，然后直接对这个Map setValue不好吗？
-        Object oldVal = getValue(key, yamlToMap);
 
         // 未找到key 不修改
         if (null == oldVal) {
@@ -203,6 +294,7 @@ public class YamlEditor {
         Yaml yaml = new Yaml(dumperOptions);
         String path = this.getClass().getClassLoader().getResource(yamlName).getPath();
         try {
+            //if (this.setValue(target, key, value)) // 这样就不用进去太深了
             if (this.setValue(yamlToMap, key, value)) {
                 yaml.dump(yamlToMap, new FileWriter(path));
                 return true;
--- src/test/java/com/fidt/yamleditor/TestEditor.java
+++ src/test/java/com/fidt/yamleditor/TestEditor.java
@@ -3,6 +3,8 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -20,7 +22,7 @@ public class TestEditor {
         System.out.println(configs.getYamlToMap("templates/configtx.yaml"));
     }
     @Test
-    public void testYamlGetValue(){
+    public void testYamlGetValue() throws Exception {
         LinkedHashMap<String,Object> map = new LinkedHashMap();
         map.put("1",1);
         map.put("2",2);
@@ -33,13 +35,18 @@ public class TestEditor {
         return;
     }
     @Test
-    public void testYamlGetValue2(){
+    public void testYamlGetValue2() throws Exception {
         LinkedHashMap<String,Object> map = new LinkedHashMap();
         map.put("1",1);
         LinkedHashMap<String,Object> map2 = new LinkedHashMap();
-        map.put("3",map2);
+        map.put("2",map2);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(2);
+        map.put("4",list);
         map2.put("1",2);
-        if (YamlEditor.getValue("3",map) instanceof Map){
+        Object o = YamlEditor.getValue("4.0",map);
+        if (YamlEditor.getValue("4.0",map) instanceof Map){
             Map map3 = (Map) YamlEditor.getValue("3",map);
             map3.put("1",5);
         }
